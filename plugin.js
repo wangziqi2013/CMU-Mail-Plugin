de1a3e7d168d13f3aa45bc703c0aec41c2d09383
wangziqi2013 <zwa47@sfu.ca>
2016-01-29 22:16:26 -0500

    First commit

---
+++ plugin.js
@@ -0,0 +1,190 @@
+// ==UserScript==
+// @name        CMU_Mail_Filter
+// @namespace   675973885@qq.com
+// @match     https://webmail.andrew.cmu.edu/src/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var unwanted_list_exact = [
+  "@piazza.com",
+  "lieyongz",
+  "sebastian",
+]
+
+var box_action_dict = {
+  "INBOX": [remove_unwanted, ], 
+  "Sent": [remove_unwanted, ], 
+}
+  
+var column_action_dict = {
+  "left_main": [add_item_to_left_column, ],
+  "right_main": [determine_box, ],
+}
+
+function add_item_to_left_column()
+{
+  
+}
+
+/*
+ * report_removed_num() - Output to the page on how many items has been hidden
+ */
+function report_removed_num(num)
+{
+  var bottom_bar = document.getElementsByClassName("paginatorInner");
+  if(bottom_bar.length != 1)
+  {
+    alert("Illegal mailbox layout!\nCannot report removed number!");
+    return;
+  }
+  
+  bottom_bar = bottom_bar[0].children[0].children[0].children[1];
+  var txt = bottom_bar.innerHTML;
+  
+  end_index = txt.indexOf("total)");
+  if(end_index == -1)
+  {
+    alert("Illegal summary layout!");
+    return;
+  }
+
+  end_index += "total".length;
+  txt_new = txt.substring(0, end_index) + ", " + num.toString() + " hidden)";
+  bottom_bar.innerHTML = txt_new;
+  
+  return;
+}
+  
+function remove_unwanted()
+{
+  var i;
+  
+  from_list = document.getElementsByClassName("fieldFrom");
+  list_len = from_list.length;
+  
+  if(list_len == 0) return;
+  
+  var removed_num = 0;
+  for(i = 0;i < from_list.length;i++)
+  {
+    from_email = from_list[i].getAttribute("title");
+    
+    var j;
+    for(j = 0;j < unwanted_list_exact.length;j++)
+    {
+      var index = from_email.indexOf(unwanted_list_exact[j]);
+      
+      if(index != -1)
+      {
+        var parent_node = from_list[i].parentNode;
+        
+        parent_node.style.display = "none";
+        // Accounting 
+        removed_num += 1;
+        // Only match for once
+        break;
+      }
+    }
+  }
+  
+  report_removed_num(removed_num);
+}
+
+/*
+ * parse_get_parameter() - Returns a mapping from key to value
+ */
+function parse_get_parameter(url_txt)
+{
+  var begin_index = url_txt.indexOf("?");
+  if(begin_index == -1) return {};
+  
+  param = url_txt.substring(begin_index + 1, url_txt.length);
+  param_list = param.split("&");
+  
+  var param_dict = {};
+  var i;
+  for(i = 0;i < param_list.length;i++)
+  {
+    key_value_pair = param_list[i].split("=");
+    if(key_value_pair.length != 2)
+    {
+      alert("Invalid request URL!");
+      return {};
+    }
+    
+    var key = key_value_pair[0];
+    var value = key_value_pair[1];
+    
+    param_dict[key] = value;
+  }
+  
+  return param_dict;
+}
+
+/*
+ * get_page_box_text() - Return the text on the page indicating current box
+ */
+function get_page_box_text()
+{
+  var folder_list = document.getElementById("currentFolderList");
+  if(folder_list != undefined)
+  {
+    var page_box_text = folder_list.children[1];
+    if(page_box_text != undefined)
+    {
+      return page_box_text.innerHTML;
+    }
+  }
+  
+  return "";
+}
+
+/*
+ * determine_box() - Determine the current mail box, if it is
+ * in the allowed list then all functions related to this mailbox
+ * is executed
+ */
+function determine_box()
+{ 
+  var is_right_box = false;
+  var page_box_text = get_page_box_text();
+  //alert(page_box_text);
+  //alert(window.location.href);
+  
+  if(page_box_text in box_action_dict)
+  {
+    box_action_list = box_action_dict[page_box_text];
+    var i;
+    for(i = 0;i < box_action_list.length;i++)
+    {
+      box_action_list[i]();
+    }
+  }
+  
+  return;
+}
+
+/*
+ * dispatch_column() - Call functions for different columns
+ */
+function dispatch_column()
+{
+  var link_text = window.location.href;
+  
+  var suffix_index = link_text.indexOf(".php");
+  var prefix_index = link_text.lastIndexOf("/");
+  
+  var column_id = link_text.substring(prefix_index + 1, suffix_index);
+  if(column_id in column_action_dict)
+  {
+    var column_action_list = column_action_dict[column_id];
+    var i;
+    for(i = 0;i < column_action_list.length;i++)
+    {
+      column_action_list[i]();
+    }
+  }
+}
+
+dispatch_column();
